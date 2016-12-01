168a3ae55e8be248041ecc5afad15707fccc1a24
Thomas Valley <mysterywisconsin@gmail.com>
2016-11-30 20:30:52 -0500

    arbitrary commit
    commision for syncing between my work machines

--- gulpfile.js
+++ gulpfile.js
@@ -42,11 +42,12 @@ gulp.task('test',function() {
 	  	if (err) {
 		    return console.log(err);
 		  }
-		  parse(data, {comment: '#'}, function(err, output){
+		  parse(data, {comment: '#', columns: true}, function(err, output){
 			 	if (err) {
 			    return console.log(err);
 			  }
-			  output[0].should.eql([
+			  Object.keys(output[0]).should.eql([
+			  	'event_id',
 			    'user_id',
 			    'convention_id',
 			    'priority',
@@ -65,10 +66,66 @@ gulp.task('test',function() {
 			    'type_id',
 			    'long_description'
 			  ]);
-			  if(output.length <= 1) {
+			  if(output.length < 1) {
 			  	console.log('Error: No Data in '+args.input_file);
 			  }
 			});
 		});
   }
+});
+
+gulp.task('submit',function() {
+	var args = yargs
+		.alias('f','input_file')
+		.argv;
+		
+  if(args.input_file) {
+  	fs.readFile('./'+args.input_file, 'utf8', function (err,data) {
+	  	if (err) {
+		    return console.log(err);
+		  }
+		  parse(data, {comment: '#', columns: true}, function(err, output){
+			 	if (err) {
+			    return console.log(err);
+			  }
+			  Object.keys(output[0]).should.eql([
+			  	'event_id',
+			    'user_id',
+			    'convention_id',
+			    'priority',
+			    'sellable',
+			    'price',
+			    'allow_schedule_conflicts',
+			    'custom_fields',
+			    'age_range',
+			    'name',
+			    'duration',
+			    'description',
+			    'max_tickets',
+			    'more_info_uri',
+			    'alternatedaypart_id',
+			    'preferreddaypart_id',
+			    'type_id',
+			    'long_description'
+			  ]);
+			  if(output.length < 1) {
+			  	console.log('Error: No Data in '+args.input_file);
+			  }
+			  
+				request.post(
+			    'https://tabletop.events/api/session',
+			    { json: creds },
+			    function (error, response, body) {
+			    	gutil.log(response.statusCode+"\n");
+		        if (!error && response.statusCode == 200) {
+		            var session_id = body.id;
+		            console.log('session '+session_id);
+		            var 
+		            	
+		        }
+			    }
+				);			  
+			});
+		});
+  }		
 });
